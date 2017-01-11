c17b3b8d40a5c5705799c7b04b85d49e996bc939
zaxophone <zaxophone@users.noreply.github.com>
2017-01-10 20:44:06 -0800

    Utilized the Dev FileAssociation feature

---
+++ Zack's Plugin/aqua.cs
@@ -0,0 +1,48 @@
+﻿using PluginAPI;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AquaConsole.FileAssociations
+{
+    class nf : IAssociatedFile
+    {
+        public string Extension
+        {
+            get
+            {
+                return "aqua";
+            }
+        }
+
+        public void CommandMethod(string file)
+        {
+            if (Utility.FileOrDirectoryExists(file))
+            {
+                List<List<string>> groups = new List<List<string>>();
+                foreach (var line in File.ReadAllLines(file))
+                {
+                    string AquaConsoleExe = (Assembly.GetExecutingAssembly().Location + "\\AquaConsole.exe");
+                    string[] readText = File.ReadAllLines(file);
+
+                    Process ac = new Process();
+                    ac.StartInfo.FileName = AquaConsoleExe;
+                    ac.StartInfo.UseShellExecute = false;
+                    ac.StartInfo.RedirectStandardInput = true;
+
+                    ac.Start();
+
+                    StreamWriter acsw = ac.StandardInput;
+                    acsw.WriteLine(line);
+                }
+            }
+            else
+                Utility.ErrorWriteLine("Warning- File is nonexistent or you have insufficient permissions to access it or your file system is corrupt or there's a system I/O error");
+        }
+    }
+}
