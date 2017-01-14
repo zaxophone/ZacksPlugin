985ce2b8268cc2533f12cc158b2e65452225652a
zaxophone <zaxophone@users.noreply.github.com>
2017-01-14 12:49:33 -0800

    Update Main.cs

--- Zack's Plugin/Main.cs
+++ Zack's Plugin/Main.cs
@@ -29,26 +29,7 @@ namespace ZacksPlugin
         //Loaded before everything else- obviously
         public void PreloadMethod()
         {
-            string ext = (".aqua");
-            string aquac = System.Reflection.Assembly.GetExecutingAssembly().CodeBase;
-            string aquaexe = (aquac + "\\AquaConsole.exe");
-            string newkeym = @"HKEY_CLASSES_ROOT\.aqua";
-            if (Registry.GetValue(newkeym, "AquaConsole", null) == null)
-            {
-                RegistryKey aquakey = Registry.ClassesRoot.CreateSubKey(ext);
-                aquakey.SetValue("", "AquaConsole");
-                aquakey.Close();
-
-                aquakey = Registry.ClassesRoot.CreateSubKey(ext + "\\Shell\\Open\\command");
-                aquakey = aquakey.CreateSubKey("command");
-
-                aquakey.SetValue("", "\"" + aquaexe + "\" \"%L\"");
-                aquakey.Close()
-            }
-            else
-            {
-                throw new NotImplementedException();
-            }
+            throw new NotImplementedException();
         }
     }
 }
